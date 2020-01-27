117f3f41dcc952a1e37eea457bf83f18172132d7
andrerosantos <andre.santos.95@hotmail.com>
2020-01-27 15:34:07 +0000

    Client v1.0

--- src/com/insure/client/Main.java
+++ src/com/insure/client/Main.java
@@ -1,7 +1,25 @@
 package com.insure.client;
 
+import com.insure.client.gen.Claim;
+import com.insure.client.gen.ClaimDataStore;
+import com.insure.client.gen.ClaimDataStoreService;
+
+import javax.xml.ws.BindingProvider;
+
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Project template - client");
+    public static void main(String args[]) throws InterruptedException{
+        ClaimDataStoreService cService = new ClaimDataStoreService();
+
+        final ClaimDataStore claim = (ClaimDataStore) cService.getClaimDataStorePort();
+
+        ((BindingProvider) claim).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                "http://146.193.7.111:8090/docstorage");
+
+        System.out.println(claim.checkIfWorking());
+
+    }
+
+    public static void runService(){
+
     }
 }
