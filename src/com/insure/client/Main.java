cf1e4c12cc3ad869737798716baba36456621569
hsemeao <hmsemeao@gmail.com>
2020-02-03 12:09:23 +0000

    New Methods

--- src/com/insure/client/Main.java
+++ src/com/insure/client/Main.java
@@ -1,14 +1,18 @@
 package com.insure.client;
 
-//Wsimport.bat -s ..\src -keep -p com.insure.client.gen "http://localhost:8090/docstorage?wsdl"
+//Wsimport.bat -s ..\src -keep -p com.insure.client.gen "http://146.193.7.121:8090/docstorage?wsdl"
 
 public class Main {
-    public static void main(String args[]) throws InterruptedException{
+    public static void main(String args[]) throws InterruptedException {
         Insured insured0 = new Insured(0);
 
         int claimID = insured0.createClaim("Test claim");
 
-        insured0.addDocument("Document 1", claimID);
+        int docID=insured0.addDocument("Document 1", claimID);
+        System.out.println(insured0.claimToString(claimID));
+        System.out.println(insured0.listDocuments(claimID));
+        System.out.println("View"+insured0.viewDocument(docID,claimID));
+
 
         insured0.addDocument("Document 2", claimID);
 
@@ -17,7 +21,6 @@ public class Main {
     }
 
     //nao me lembro para que e que isto servia
-    public static void runService(){
-
+    public static void runService() {
     }
 }
